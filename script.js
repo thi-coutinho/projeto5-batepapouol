1c3e68b6247be2f257d11ebfd05f43bd431f3a4b
thiagocsantos <thi.coutinho.santos@gmail.com>
2022-10-29 14:31:34 -0300

    feat: projeto pronto sem o bonus

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const linkGetMessages = "https://mock-api.driven.com.br/api/v6/uol/messages"
+const linkPostMessages = linkGetMessages
 const linkPostJoinServer = "https://mock-api.driven.com.br/api/v6/uol/participants"
 const linkPostStatus = "https://mock-api.driven.com.br/api/v6/uol/status"
+let lastMessage; // para conferir se precisa recarregar a página
 let userName;
 let user;
 let pergunta = "Qual o seu lindo nome?";
+let to = "Todos";
 
 function perguntaNome(response) {
     userName = prompt(pergunta)
     pergunta = "Por favor digite outro nome, pois este já está em uso"
     user = { name: userName }
-    console.log(user)
     axios.post(linkPostJoinServer, user)
         .then(enterRoom)
         .catch(perguntaNome)
@@ -29,9 +31,28 @@ function enterRoom() {
     }
     setInterval(keepOnline, 5000)
     // renderiza mensagens
+    setInterval(getAllMessages, 3000);
+
+}
+
+function getAllMessages() {
     axios.get(linkGetMessages)
         .then(rendMenssages)
-        .catch(console.log)
+        .catch(console.log);
+}
+
+function sendMessage() {
+    let text = document.querySelector("input");
+    let ObjMessage = {
+        from: userName,
+        to: "Todos",
+        text: text.value,
+        type: "message"
+    }
+    axios.post(linkPostMessages, ObjMessage)
+        .then(getAllMessages)
+        .catch(window.location.reload)
+    text.value = ""
 
 }
 
@@ -62,25 +83,46 @@ fazer função enviar mensagem
 
 function rendMenssages(resposta) {
     let messages = resposta.data
+    // cancel if lastMessage is the same as last call
+    if (JSON.stringify(lastMessage) == JSON.stringify(messages[messages.length - 1])) {
+        console.log("não precisa imprimir ");
+        return
+    }
+    // update lastMessage
+    lastMessage = messages[messages.length - 1];
     const ulMessages = document.querySelector(".list-messages")
+
+
     for (let i = 0; i < messages.length; i++) {
         if (messages[i].to === "Todos" || messages[i].to === userName) {
             let classes = "";
-            
-            classes+= messages[i].to === userName? " private" :"";
-            classes+= messages[i].type === "status"? " status": "";
-            
-            ulMessages.innerHTML += `
-            <li class="message${classes}">
-                <span class="time">(${messages[i].time})</span>
-                <span class="sender">  <strong> ${messages[i].from}</strong>  ${messages[i].text}</span>
-                <span class="message-content"></span>
-            </li>
-            `
+
+            classes += messages[i].type === "private_message" ? "private" : "";
+            classes += messages[i].type === "status" ? "status" : "";
+
+            ulMessages.innerHTML += liMessage(classes, messages, i)
         }
     }
-    const lastMessage = document.querySelector(".list-messages li:last-child")
-    console.log(lastMessage)
-    lastMessage.scrollIntoView()
+    document.querySelector(".list-messages li:last-child")
+        .scrollIntoView()
 
+}
+
+function liMessage(classes, messages, i) {
+    let inner = " para ";
+    let outer = ": "
+    if (classes === "status") {
+        messages[i].to = "";
+        inner = "";
+        outer = "";
+    } else if (classes === "private_message") {
+        inner = " reservadamente" + inner
+    }
+    return `
+            <li class="message ${classes}">
+                <span class="time">(${messages[i].time})</span>
+                <span class="sender"><strong> ${messages[i].from}</strong>${inner}<strong>${messages[i].to}</strong>${outer}</span>
+                <span class="message-content">${messages[i].text}</span>
+            </li>
+            `;
 }
